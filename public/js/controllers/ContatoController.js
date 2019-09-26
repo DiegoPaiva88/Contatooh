fc8f3f28e6d8f10ae798d91ce0b9927e873577df
Diego Paiva <paivahenrique88@hotmail.com>
2019-09-26 20:54:59 -0300

    opcao de salvar novos contatos

--- public/js/controllers/ContatoController.js
+++ public/js/controllers/ContatoController.js
@@ -1,5 +1,35 @@
 angular.module('contatooh').controller('ContatoController',
-    function ($scope, $routeParams) {
+    function ($scope, $routeParams, $resource) {
 
-        console.log($routeParams.contatoId);
+        var Contato = $resource('/contatos/:id');
+
+        if ($routeParams.contatoId) {
+            Contato.get({ id: $routeParams.contatoId },
+                function (contato) {
+                    $scope.contato = contato;
+                },
+                function (erro) {
+                    $scope.mensagem = {
+                        texto: 'Contato não existe. Novo contato.'
+                    };
+                    //console.log(erro);
+                }
+            );
+        } else {
+            $scope.contato = new Contato();
+        }
+
+        $scope.salva = function () {
+            $scope.contato.$save()
+                .then(function () {
+                    $scope.mensagem = { texto: 'Salvo com sucesso' };
+                    //limpa o formulario
+                    $scope.contato = new Contato();
+                })
+                .catch(function (erro) {
+                    $scope.mensagem = { texto: 'Não foi possível salvar' };
+                });
+        }
+
+        //console.log($routeParams.contatoId);
     });
--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -3,29 +3,37 @@ angular.module('contatooh').controller('ContatosController',
 
         $scope.contatos = [];
 
-        var Contato = $resource('/contatos');
+        $scope.filtr = '';
+
+        $scope.mensagem = { texto: '' };
+
+        var Contato = $resource('/contatos/:id');
 
         function buscarContatos() {
             Contato.query(function (contatos) {
                 $scope.contatos = contatos;
+                $scope.mesagem = {};
             },
                 function (erro) {
-                    console.log("Não foi possível obter a lista de contatos");
                     console.log(erro);
+                    $scope.mesagem = {
+                        texto: 'Não foi possivel obter a lista'
+                    };
                 }
             );
         }
         buscarContatos();
 
         $scope.remove = function (contato) {
-            var promise = Contato.delete({ id: contato._id }).$promise;
-            promise.then(buscarContatos)
-                .catch(function (erro) {
-                    console.log("Não foi possível remover o contato");
+            Contato.delete({ id: contato._id },
+                buscarContatos,
+                function (erro) {
+                    $scope.mesagem = {
+                        texto: 'Não foi possivel obter a lista'
+                    };
                     console.log(erro);
-                });
+                }
+            );
         };
-
-        $scope.filtr = '';
 
     });
